e1c797254cdf9efb67343fc106c0bb43f8e5f72c
Aurélien Pelletier <aurelien.pelletier@gmail.com>
2011-12-16 14:37:21 +0100

    Formating

--- fooBarQix/src/main/java/net/toutantic/foobarqix/CodeStoryFooBarQix.java
+++ fooBarQix/src/main/java/net/toutantic/foobarqix/CodeStoryFooBarQix.java
@@ -2,21 +2,21 @@ package net.toutantic.foobarqix;
 
 public class CodeStoryFooBarQix extends NumberConversionEngine {
 
-    private final static String FOO="Foo";
-    private final static String BAR="Bar";
-    private final static String QIX="Qix";
-    
-    @Override
-    public void convert() {
-	ifNumberCanBeDivideBy(3).append(FOO);
-	ifNumberCanBeDivideBy(5).append(BAR);
-	ifNumberCanBeDivideBy(7).append(QIX);
-	while (nextDigit()) {
-	    ifDigitIs(3).append(FOO);
-	    ifDigitIs(5).append(BAR);
-	    ifDigitIs(7).append(QIX);
-	}
+	private final static String FOO = "Foo";
+	private final static String BAR = "Bar";
+	private final static String QIX = "Qix";
 
-	ifNoRuleApplied().append(numberToString());
-    }
+	@Override
+	public void convert() {
+		ifNumberCanBeDivideBy(3).append(FOO);
+		ifNumberCanBeDivideBy(5).append(BAR);
+		ifNumberCanBeDivideBy(7).append(QIX);
+		while (nextDigit()) {
+			ifDigitIs(3).append(FOO);
+			ifDigitIs(5).append(BAR);
+			ifDigitIs(7).append(QIX);
+		}
+
+		ifNoRuleApplied().append(numberToString());
+	}
 }
